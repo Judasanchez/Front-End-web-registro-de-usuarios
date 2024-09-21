142777e680b9b1f2e19e9b62202caa72c03092a1
JulyanaLozano <josephtemi@hotmail.com>
2024-09-21 12:59:19 -0500

    Modulo principal, login boostraap, modulo de registro de ususarios y un dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { RegisterComponent } from './components/register/register.component';
+import { MainComponent } from './components/main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/main', pathMatch: 'full' },
+    { path: 'main', component: MainComponent },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
+    }
+];
